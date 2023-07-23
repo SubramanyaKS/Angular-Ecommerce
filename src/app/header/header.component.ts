a67bfc826dadc88b9d335f098849442771aae490
Subramanya K S <subramanyaks2205@gmail.com>
2023-07-23 16:38:33 +0530

    updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
+    this.route.events.subscribe((val:any)=>{
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          console.log("Inside Seller Area");          
+        }
+        else{
+          console.log("Outside seller Area");
+        }
+      }
+    })
   }
 
 }
